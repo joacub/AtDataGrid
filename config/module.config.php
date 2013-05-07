1e7a247b9878dc5465e5b11a0afed873cd11dd0d
Johan <joacub@gmail.com>
2013-05-07 16:29:29 +0200

    mejoras

--- config/module.config.php
+++ config/module.config.php
@@ -6,4 +6,11 @@ return array(
             __DIR__ . '/../view',
         ),
     ),
+	'asset_manager' => array(
+		'resolver_configs' => array(
+			'paths' => array(
+				__DIR__ . '/../public/',
+			)
+		)
+	),
 );
